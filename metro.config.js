f6bdaac4ecfdd71325c17b4bcc91bc146f8d4ced
Nishant Munjal <timepasscoding1994@gmail.com>
2026-08-06 14:41:00 +0530

    Fix Expo startup compatibility

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,10 @@
+const os = require('os');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+
+if (typeof os.availableParallelism !== 'function') {
+  os.availableParallelism = () => os.cpus()?.length ?? 1;
+}
 
 const config = getDefaultConfig(__dirname);
 
